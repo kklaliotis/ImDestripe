f061e6f7d238343747c7ddda654e73713c090574
Katherine Laliotis <kklaliotis@gmail.com>
2024-10-25 13:40:30 -0400

    fixed to test interpolation only

--- destripe.py
+++ destripe.py
@@ -15,6 +15,7 @@ from astropy import wcs
 from scipy import ndimage
 import compareutils
 import re
+import sys
 
 # KL: Placeholders, some of these should be input arguments or in a config or something
 input_dir = '/fs/scratch/PCON0003/cond0007/anl-run-in-prod/simple/'
@@ -24,7 +25,8 @@ filter = 'H158'
 model_params = {'constant': 1, 'linear': 2}
 permanent_mask = '/users/PCON0003/cond0007/imcom/coadd-test-fall2022/permanent_mask_220730.fits'
 outfile = '/fs/scratch/PCON0003/klaliotis/destripe/destripe_'+filter+'_out.txt'
-tempfile = '/tmp/klaliotis-tmp/'
+tempfile = '/fs/scratch/PCON0003/klaliotis/destripe/' #temporary temp file so that i can check these things
+#tempfile = '/tmp/klaliotis-tmp/'
 s_in  = 0.11
 
 
@@ -52,6 +54,7 @@ class sca_img:
         image: the SCA image (4088x4088) (KL: np array or pd dataframe?_
         shape: shape of the image
         wcs: the astropy.wcs object associated with this SCA
+        mask: the full pixel mask that is used on this image. Is correct only after running both apply mask methods
     Functions:
     apply_noise: apply the appropriate lab noise frame to the SCA image
     get_overlap: figure out which other SCA images overlap this one
@@ -68,6 +71,7 @@ class sca_img:
         self.obsid = obsid
         self.scaid = scaid
         file.close()
+        self.mask = np.ones(self.shape)
 
     def apply_noise(self):
         noiseframe = np.copy(fits.open(labnoise_prefix+self.obsid+'_'+self.scaid+'.fits')['PRIMARY'].data)
@@ -77,6 +81,7 @@ class sca_img:
     def apply_permanent_mask(self):
         pm = np.copy(fits.open(permanent_mask)[0].data[int(self.scaid)-1])
         self.image = self.image * pm
+        self.mask = self.mask * pm
         return self.image
 
     def apply_object_mask(self):
@@ -85,6 +90,7 @@ class sca_img:
             for j in range(self.shape[1]):
                 if self.image[i,j]>=1.5*median:
                     self.image[i-2:i+2,j-2:j+2]=0
+                    self.mask[i - 2:i + 2, j - 2:j + 2] = 0
         return self.image
 
 
@@ -171,14 +177,19 @@ def get_scas(filter, prefix):
     """
     n_scas = 0
     all_scas = []
+    all_wcs = []
     for f in glob.glob(input_dir+prefix+filter+'_*'):
         n_scas += 1
         m = re.search(r'(\w\d+)_(\d+)_(\d+)', f)
         if m:
-            obsfile = str(m.group(0))
-            all_scas.append(obsfile)
+            this_obsfile = str(m.group(0))
+            all_scas.append(this_obsfile)
+            this_file = fits.open(f)
+            this_wcs = wcs.WCS(this_file['SCI'].header)
+            all_wcs.append(this_wcs)
+            this_file.close()
     write_to_file('N SCA images in this mosaic: ' + str(n_scas))
-    return all_scas
+    return all_scas, all_wcs
 
 def interpolate_image(target_wcs, ref_wcs, ref_image):
     """
@@ -192,27 +203,20 @@ def interpolate_image(target_wcs, ref_wcs, ref_image):
     interp_image = ndimage.map_coordinates(ref_image, [[x_target], [y_target]])[0,:,:]
     return interp_image
 
-def check_overlap(target_wcs, ref_wcs):
-    """
-    Check if a reference SCA overlaps a target SCA
-    :param target_sca: an SCA object
-    :param ref_sca:
-    :return: Bool; whether the two SCAs overlap
-    """
-    wcs_list = [target_wcs,ref_wcs]
-    ov_mat = compareutils.get_overlap_matrix(wcs_list, verbose=True)
-    return np.any(ov_mat[0, 1])
 
 
 ############################ Main Sequence ############################
 
-all_scas = get_scas(filter, image_prefix)
+all_scas, all_wcs = get_scas(filter, image_prefix)
 print(len(all_scas), " SCAs in this mosaic")
+print(len(all_wcs), "WCS in the list (if not same as above, we have a problem)")
 
+ov_mat = compareutils.get_overlap_matrix(all_wcs, verbose=True) #an N_wcs x N_wcs matrix containing fractional overlap
+print("Overlap matrix complete")
 # In this chunk of code, we iterate through all the SCAs and create interpolated
-# versions of them from all the other SCAs
-for i,sca in enumerate(all_scas):
-    m = re.search(r'_(\d+)_(\d+)', sca)
+# versions of them from all the other SCAs that overlap them
+for i,sca_a in enumerate(all_scas):
+    m = re.search(r'_(\d+)_(\d+)', sca_a)
     obsid_A = m.group(1)
     scaid_A = m.group(2)
     print('Img A: ' + obsid_A + '_' + scaid_A)
@@ -222,80 +226,29 @@ for i,sca in enumerate(all_scas):
     I_A.apply_permanent_mask()
 
     I_A_interp = np.zeros(I_A.shape)
-    n_BinA = 0
+    N_eff = np.zeros(I_A.shape)
 
-    for sca_b in all_scas:
+    print('Starting interpolation for SCA A')
+    sys.stdout.flush()
+
+    for j,sca_b in enumerate(all_scas):
         m = re.search(r'_(\d+)_(\d+)', sca_b)
         obsid_B = m.group(1)
         scaid_B = m.group(2)
 
-        if obsid_B != obsid_A:
+        if obsid_B != obsid_A and ov_mat[i,j] != 0:
             I_B = sca_img(obsid_B, scaid_B)
             I_B.apply_noise()
             I_B.apply_permanent_mask()
             I_B.apply_object_mask()
+            I_A_interp += interpolate_image(I_A.w, I_B.w, I_B.image)
+            N_eff += I_B.mask
 
-            if check_overlap(I_A.w, I_B.w):
-                print('Overlap True, Image B: '+obsid_B+'_'+scaid_B)
-                I_A_interp += interpolate_image(I_A.w, I_B.w, I_B.image)
-                n_BinA += 1
-
-    hdu = fits.PrimaryHDU(I_A_interp/n_BinA)
-    hdu.writeto(tempfile+obsid_A+'_'+scaid_A+'_interp.fits', overwrite=True)
-    print(tempfile+obsid_A+'_'+scaid_A+'_interp.fits created \n')
+    hdu = fits.PrimaryHDU(np.divide(I_A_interp, N_eff))
+    hdu.writeto(tempfile+filter+'/'+obsid_A+'_'+scaid_A+'_interp.fits', overwrite=True)
+    print(tempfile+filter+'/'+obsid_A+'_'+scaid_A+'_interp.fits created \n')
     print('Remaining SCAs: ' + str(len(all_scas)-1-i) + '\n')
 
-# # Initiate parameters
-# def destripe(tolerance=1e-10, max_it=100):
-#     params = ds_parameters('constant', 4088)
-#     resid = params.params.copy()
-#     Ad = params.params.copy()
-#
-#     tolerance = tolerance
-#     max_it = max_it
-#
-#     it = 0  # iteration counter
-#     diff = 1.0
-#     tol_hist_jac = []
-#
-#     p = params.params.copy()
 
-#     # Initial residual r0 and initial search direction d0
-#     resid[1:-1, 1:-1] = -b[1:-1, 1:-1] - A(p, dx, dy)
-#     d = resid.copy()
-#
-#     while (diff > tolerance):
-#         if it > max_it:
-#             print('\nSolution did not converged within the maximum'
-#                   ' number of iterations'
-#                   f'\nLast l2_diff was: {diff:.5e}')
-#             break
-#
-#         # Laplacian of the search direction.
-#         Ad[1:-1, 1:-1] = A(d, dx, dy)
-#         # Magnitude of jump.
-#         alpha = np.sum(r * r) / np.sum(d * Ad)
-#         # Iterated solution
-#         pnew = p + alpha * d
-#         # Intermediate computation
-#         beta_denom = np.sum(r * r)
-#         # Update the residual.
-#         r = r - alpha * Ad
-#         # Compute beta
-#         beta = np.sum(r * r) / beta_denom
-#         # Update the search direction.
-#         d = r + beta * d
-#
-#         diff = l2_diff(pnew, p)
-#         tol_hist_jac.append(diff)
-#
-#         # Get ready for next iteration
-#         it += 1
-#         np.copyto(p, pnew)
-#
-#
-#     else:
-#         print(f'\nThe solution converged after {it} iterations')
-# # go through CGD algorithm using all_scas and all_scas_interp lists, optimizing params
 
 
